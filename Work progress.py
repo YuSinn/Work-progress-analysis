9fc153932e94dae15c65261e9bd735e85c3e7780
Vladut Gabriel Alexandru <alexandru.vladut.gabriel97@gmail.com>
2022-08-26 12:55:45 +0200

    Solved bugs

--- Work progress.py
+++ Work progress.py
@@ -10,19 +10,26 @@ import numpy as np
 months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre','Noviembre', 'Diciembre']
 en, fe, ma, ab, may, jun, jul, ag, se, oc, no, dic = list(range(1,32)), list(range(1,29)), list(range(1,32)), list(range(1, 31)), list(range(1, 32)), list(range(1, 31)), list(range(1, 32)), list(range(1, 32)), list(range(1, 31)), list(range(1, 32)), list(range(1, 31)), list(range(1, 32))
 
-
 with open('Progress.csv', 'a', newline='') as file:
     writer = csv.writer(file)
     dias = int(input('Cuantos dias vas a introducir? '))
     for i in range(1,dias+1):
-        dia = input("Introduce el dia: ")
-        if dia in range(1,32):
-            mes = input("Introduzca el mes: ")
-        else:
-            dia = input("Por favor introduzca un numero de 1 a 31: ")
+        mes = input("Introduce un mes: ")
+        mes = mes.capitalize()
         if mes in months:
-            tran = input("Introduce el numero de transcripciones: ")
+            dia = int(input("Introduce el dia: "))
+            if dia in range(1,32):
+                tran = int(input("Introduce el numero de transcripciones: "))
+            else:
+                dia = int(input("Los meses tienen hasta 31 dias, por favor introduzca de nuevo el dia! "))
+                tran = int(input("Introduce el numero de transcripciones: "))
         else:
-            mes = input("Por favor introduzca un mes valido! ")
-            tran = tran = input("Introduce el numero de transcripciones: ")    
+            mes = input("Introduce un mes valido por favor! ")
+            mes = mes.capitalize()      
+            dia = int(input("Introduce el dia: "))
+            if dia in range(1,32):
+                tran = int(input("Introduce el numero de transcripciones: "))
+            else:
+                dia = int(input("Los meses tienen hasta 31 dias, por favor introduzca de nuevo el dia! "))
+                tran = int(input("Introduce el numero de transcripciones: "))   
         writer.writerow([dia, mes, tran])
