83d34a81440f939d13260ac87c93a090f0f8e954
Vladut Gabriel Alexandru <alexandru.vladut.gabriel97@gmail.com>
2022-08-31 11:37:53 +0200

    Update to python file

--- Work progress.py
+++ Work progress.py
@@ -13,23 +13,41 @@ en, fe, ma, ab, may, jun, jul, ag, se, oc, no, dic = list(range(1,32)), list(ran
 with open('Progress.csv', 'a', newline='') as file:
     writer = csv.writer(file)
     dias = int(input('Cuantos dias vas a introducir? '))
-    for i in range(1,dias+1):
-        mes = input("Introduce un mes: ")
-        mes = mes.capitalize()
-        if mes in months:
-            dia = int(input("Introduce el dia: "))
-            if dia in range(1,32):
-                tran = int(input("Introduce el numero de transcripciones: "))
+    if dias > 0:
+        for i in range(1,dias+1):
+            mes = input("Introduce el mes: ")
+            mes = mes.capitalize()
+            if mes in months:
+                dia = int(input("Introduce el dia: "))
+                if dia in range(1,32):
+                    tran = int(input("Introduce el numero de transcripciones: "))
+                else:
+                    dia = int(input("Los meses tienen hasta 31 dias, por favor introduzca de nuevo el dia! "))
+                    tran = int(input("Introduce el numero de transcripciones: "))
             else:
-                dia = int(input("Los meses tienen hasta 31 dias, por favor introduzca de nuevo el dia! "))
-                tran = int(input("Introduce el numero de transcripciones: "))
-        else:
-            mes = input("Introduce un mes valido por favor! ")
-            mes = mes.capitalize()      
-            dia = int(input("Introduce el dia: "))
-            if dia in range(1,32):
-                tran = int(input("Introduce el numero de transcripciones: "))
-            else:
-                dia = int(input("Los meses tienen hasta 31 dias, por favor introduzca de nuevo el dia! "))
-                tran = int(input("Introduce el numero de transcripciones: "))   
-        writer.writerow([dia, mes, tran])
+                mes = input("Introduce un mes valido por favor! ")
+                mes = mes.capitalize()      
+                dia = int(input("Introduce el dia: "))
+                if dia in range(1,32):
+                    tran = int(input("Introduce el numero de transcripciones: "))
+                else:
+                    dia = int(input("Los meses tienen hasta 31 dias, por favor introduzca de nuevo el dia! -> "))
+                    tran = int(input("Introduce el numero de transcripciones: ")) 
+            print('~~~ \n ~~~\n~~~')  
+            writer.writerow([dia, mes, tran])
+    else:
+        pass
+
+work_df = pd.read_csv('Progress.csv')
+
+def work_analisys(df):
+    print('           ############ Work progress analisys ############           ')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('1.) The average transcriptions for ' + str(work_df['Month'][0]) + ' is ' + str(np.average(work_df['Transcriptions'])))
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('2.) Total transcriptions in ' + str(work_df['Month'][0]) +' is ' +str(np.sum(work_df['Transcriptions'])) +' of ' + str(1890*np.count_nonzero(work_df['Transcriptions'])))
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('3.) The objective of the month was surpased with ' + str(np.sum(work_df['Transcriptions'])-1890*np.count_nonzero(work_df['Transcriptions'])) + ' transcriptions')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('4.) The revenue for ' + str(work_df.Month[0]) + ' is ' + str(round((np.sum(work_df['Transcriptions'])-1890*np.count_nonzero(work_df['Transcriptions']))*0.028,2)) +'€')      
+work_analisys(work_df)
