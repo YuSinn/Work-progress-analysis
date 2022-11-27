d3d78c1bc65bddd69961fb71285fa543bb4ff20a
Vladut Gabriel Alexandru <alexandru.vladut.gabriel97@gmail.com>
2022-11-27 20:32:41 +0100

    Update

--- Work progress.py
+++ Work progress.py
@@ -8,9 +8,13 @@ from Write_csv import *
 
 
 months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre','Noviembre', 'Diciembre']
+weekDays = ['Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday']
 en, fe, ma, ab, may, jun, jul, ag, se, oc, no, dic = list(range(1,32)), list(range(1,29)), list(range(1,32)), list(range(1, 31)), list(range(1, 32)), list(range(1, 31)), list(range(1, 32)), list(range(1, 32)), list(range(1, 31)), list(range(1, 32)), list(range(1, 31)), list(range(1, 32))
-
-mes_to_work = input('Que mes quieres editar? ')
+if input('Quieres introducir un nuevo mes? (Si/No) ') == 'Si':
+    new_month(input('Introduzca el nuevo mes: '))
+else:
+    pass
+mes_to_work = input('Que mes quieres editar? ') + '.csv'
 
 with open(mes_to_work, 'a', newline='') as file:
     writer = csv.writer(file)
@@ -23,25 +27,30 @@ with open(mes_to_work, 'a', newline='') as file:
                 dia = int(input("Introduce el dia: "))
                 if dia in range(1,32):
                     tran = int(input("Introduce el numero de transcripciones: "))
+                    if(tran > 0):
+                        weekDay = input("Introduce de que dia de la semana son las transcripciones")
                 else:
                     dia = int(input("Los meses tienen hasta 31 dias, por favor introduzca de nuevo el dia! "))
                     tran = int(input("Introduce el numero de transcripciones: "))
+                    weekDay = input("Introduce de que dia de la semana son las transcripciones")
             else:
                 mes = input("Introduce un mes valido por favor! ")
                 mes = mes.capitalize()      
                 dia = int(input("Introduce el dia: "))
                 if dia in range(1,32):
                     tran = int(input("Introduce el numero de transcripciones: "))
+                    if(tran > 0):
+                        weekDay = input("Introduce de que dia de la semana son las transcripciones")
                 else:
                     dia = int(input("Los meses tienen hasta 31 dias, por favor introduzca de nuevo el dia! -> "))
-                    tran = int(input("Introduce el numero de transcripciones: ")) 
+                    tran = int(input("Introduce el numero de transcripciones: "))
+                    weekDay = input("Introduce de que dia de la semana son las transcripciones") 
             print('~~~ \n ~~~\n~~~')  
-            writer.writerow([dia, mes, tran])
+            writer.writerow([dia, mes, tran, weekDay])
     else:
         pass
 
 work_df = pd.read_csv(mes_to_work)
-
 def work_analisys(df):
     if work_df['Month'][0] == 'Agosto':
         print('           ############ Work progress analisys ############           ')
@@ -58,13 +67,9 @@ def work_analisys(df):
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         print('1.) The average transcriptions for ' + str(work_df['Month'][0]) + ' is ' + str((int(round(np.average(work_df['Transcriptions']),0))))+'.')
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        print('2.) Total transcriptions in ' + str(work_df['Month'][0]) +' is ' +str(np.sum(work_df['Transcriptions'])) +' of ' + str(2227*np.count_nonzero(work_df['Transcriptions']))+'.')
+        print('2.) Total transcriptions in ' + str(work_df['Month'][0]) +' is ' +str(np.sum(work_df['Transcriptions'])) +' of ' + str(2227*np.count_nonzero(work_df[work_df['WeekDay'] != 'Friday']['Transcriptions'])+1890*np.count_nonzero(work_df[work_df['WeekDay'] == 'Friday']['Transcriptions']))+'.')
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        print('3.) The objective of the month was surpased with ' + str(np.sum(work_df['Transcriptions'])-2227*np.count_nonzero(work_df['Transcriptions'])) + ' transcriptions.')
+        print('3.) The objective of the month was surpased with ' + str((np.sum(work_df[work_df['WeekDay'] != 'Friday']['Transcriptions'])-2227*np.count_nonzero(work_df[work_df['WeekDay'] != 'Friday']['Transcriptions'])+np.sum(work_df[work_df['WeekDay'] == 'Friday']['Transcriptions'])-1890*np.count_nonzero(work_df[work_df['WeekDay'] == 'Friday']['Transcriptions']))) + ' transcriptions.')
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        print('4.) The revenue for ' + str(work_df.Month[0]) + ' is ' + str(round((np.sum(work_df['Transcriptions'])-2227*np.count_nonzero(work_df['Transcriptions']))*0.028,2)) +'€.')
+        print('4.) The revenue for ' + str(work_df.Month[0]) + ' is ' + str(round((np.sum(work_df[work_df['WeekDay'] != 'Friday']['Transcriptions'])-2227*np.count_nonzero((work_df[work_df['WeekDay'] != 'Friday']['Transcriptions']))+np.sum(work_df[work_df['WeekDay'] == 'Friday']['Transcriptions'])-1890*np.count_nonzero((work_df[work_df['WeekDay'] == 'Friday']['Transcriptions'])))*0.028,2)) +'€.')
 work_analisys(work_df)
-if input('Quieres introducir un nuevo mes? (Si/No) ') == 'Si':
-    new_month(input('Introduzca el nuevo mes: '))
-else:
-    pass
--- Write_csv.py
+++ Write_csv.py
@@ -3,5 +3,5 @@ import csv
 def new_month(month):
     with open(month, 'w', newline='') as f:
         w = csv.writer(f)
-        w.writerow(['Day', 'Month', 'Transcriptions'])
+        w.writerow(['Day', 'Month', 'Transcriptions','WeekDay'])
         f.close()
